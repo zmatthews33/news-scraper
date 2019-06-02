8dd584d2e911a825820a029c593a024828c5b006
whatevermane <zmatt33@gmail.com>
2019-06-02 09:19:54 -0500

    reconfig server.js

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const handlebars = require("express-handlebars");
-const morgan = require("morgan");
 
-let PORT = process.env.PORT || 3000;
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news_scraper";
+const PORT = process.env.PORT || 3000;
 
-//Init Express
 const app = express();
 
-app.use(morgan("dev"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+const routes = require("./controllers/index");
+app.use(routes);
 
-let db = mongoose.connection;
-db.on("error", error => {
-  console.log("Connection error ${error}");
+mongoose.connect("mongodb://localhost:27017/newsScraper", {
+  useNewUrlParser: true
 });
-require("./controllers/index.js")(app);
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`App running on port ${PORT}.`));
